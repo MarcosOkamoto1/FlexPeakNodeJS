285c79ef43a7a5dca110d736f44ddafff3d8fbe5
Marcos Okamoto <marcosviniciusokamoto2003@outlook.com>
2025-06-14 08:37:26 -0400

    Aula NodeFleaxPeak]

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+//http://localhost:3000/
+app.get("/ilhadossonhos", (req, res) => {
+  res.sendFile(path.resolve("ilhadossonhos.html"));
+});
+
+app.get("/flex", (req, res) => {
+  res.status(200).send("Bem-vindo a FlexPeak!");
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.resolve("404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App está rodando na porta: ${port}`);
+});
