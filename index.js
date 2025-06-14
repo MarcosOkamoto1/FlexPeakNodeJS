8abbf7f5507f9ccad9fb6698b564a3ebef4ffc1f
Marcos Okamoto <marcosviniciusokamoto2003@outlook.com>
2025-06-14 09:16:08 -0400

    Configura o DotEnv

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('dotenv').config
+require('dotenv').config()
 const path = require("path");
 
 const app = express();
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.resolve("404.html"));
 });
 
-app.listen(port, () => {
-  console.log(`App está rodando na porta: ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App está rodando na porta: ${process.env.PORT}`);
 });
