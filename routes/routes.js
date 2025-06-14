20fc49bc2ce9c3356894a22faf8b234f9a5ab95b
Marcos Okamoto <marcosviniciusokamoto2003@outlook.com>
2025-06-14 11:45:12 -0400

    Configurando Rotas

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ router.get("/", (req, res) => {
   res.send("Aplicação em construção");
 });
 
-app.use((req, res, next) => {
+router.use((req, res, next) => {
   res.status(404).send("404 Não Encontrado");
 });
 
