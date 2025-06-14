e8020c3d45e150f82113402f96f70f9f3354ce61
Marcos Okamoto <marcosviniciusokamoto2003@outlook.com>
2025-06-14 11:55:59 -0400

    Configura o caminho /flex para o json people.json

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Aplicação em construção");
 });
 
+router.get("/flex", (req, res) => {
+  res.sendFile(path.resolve('./people.json'));
+});
+
 router.use((req, res, next) => {
   res.status(404).send("404 Não Encontrado");
 });
 
-module.exports = router
+module.exports = router;
